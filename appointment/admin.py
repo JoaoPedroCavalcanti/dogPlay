be357b61fe8a9fb64f7600084b66849dcc31e00b
JoaoPedroCavalcanti <joao.cavalcanti.703@gmail.com>
2024-07-22 17:40:04 -0300

    Appointment Model created and add to admin. Migrations made

--- appointment/admin.py
+++ appointment/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Appointment
 
-# Register your models here.
+class AppointmentAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Appointment, AppointmentAdmin)
+
+# Or
+# @admin.register(Appointment)
+# class AppointmentAdmin(admin.ModelAdmin):
+#     ...
+
+
--- appointment/models.py
+++ appointment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Appointment(models.Model):
+    
+    def __str__(self):
+        strDate = str(self.date)
+        strTime = str(self.time)
+        nameItself = strDate + ' | ' + strTime+'h'
+        return nameItself
+    
+    time = models.TimeField()
+    date = models.DateField()
