6952a8f08ded9d4f3c57976b34907533080fb743
JoaoPedroCavalcanti <joao.cavalcanti.703@gmail.com>
2024-07-22 10:05:29 -0300

    Using block in HTML and change the route of appointConfirmation to use the id

--- appointment/urls.py
+++ appointment/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from appointment import views
+from . import views
+# from appointment import views
+
 
 
 urlpatterns = [
     path('', views.appointmentHome),
-    path('123/', views.appointmentConfirmation),
+    path('<int:id>/', views.appointmentConfirmation),
 ]
--- appointment/views.py
+++ appointment/views.py
@@ -5,5 +5,5 @@ from django.shortcuts import render
 def appointmentHome(req):
     return render(req, 'appointment/pages/appointmentHome.html')
 
-def appointmentConfirmation(req):
+def appointmentConfirmation(req, id):
     return render(req, 'appointment/pages/appointmentConfirmation.html')
