b28a1e090a9560a1e8ec4535323862c1355be2f6
JoaoPedroCavalcanti <joao.cavalcanti.703@gmail.com>
2024-07-23 11:12:05 -0300

    Changing function in view to data. Using get)object_or_404 and make the logic to page 404 error.

--- appointment/views.py
+++ appointment/views.py
@@ -1,31 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Appointment
-# To import dateAndTimeFuncion that is in another file
-import sys
-import os
-
-# Add the utils/appointment directory to the system path
-appointment_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../utils/appointment'))
-sys.path.append(appointment_path)
-
-# Now import the function
-from randomDateAndTime import generate_random_date_time, generate_random_data
-
-# Use the function
-# a = generate_random_date_time()
-# print(a)
+from django.http import Http404
 
 def appointmentHome(req):
+    
     appointments = Appointment.objects.all().order_by('-id')
     return render(req, 'appointment/pages/appointmentHome.html', context={
         'datesAndTimes': appointments
     })
 
 def appointmentConfirmation(req, id):
-    appointments = Appointment.objects.filter(id = id)
+    
+    appointment = get_object_or_404(Appointment, pk = id)   
     return render(req, 'appointment/pages/appointmentConfirmation.html', context={
-        # 'dataConfirmation': generate_random_data()
-        'dataConfirmation': appointments.first()
+        'dataConfirmation': appointment
     })
 
 
