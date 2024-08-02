d1c9358dac326fd09558f9ab393d9ce3a26e9acf
JoaoPedroCavalcanti <joao.cavalcanti.703@gmail.com>
2024-08-02 18:59:33 -0300

    html otimization

--- appointment/admin.py
+++ appointment/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import Appointment
 
 class AppointmentAdmin(admin.ModelAdmin):
-    ...
+    list_display = ['id', 'date',]
+    search_fields = ['id', 'date', 'time',]
 
 admin.site.register(Appointment, AppointmentAdmin)
 
