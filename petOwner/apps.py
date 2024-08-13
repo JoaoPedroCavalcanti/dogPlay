7cde017276c666a9adb2d1553d547107a3892118
JoaoPedroCavalcanti <joao.cavalcanti.703@gmail.com>
2024-08-12 21:58:34 -0300

    Signal to call the function to create profile when a user is created

--- petOwner/apps.py
+++ petOwner/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class PetownerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'petOwner'
+
+    def ready(self, *args, **kwargs) -> None:
+        import petOwner.signals
+        super_ready = super().ready(*args, **kwargs)
+        return super_ready
