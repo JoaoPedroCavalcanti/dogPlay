811604f968565126476253cc22d4384c8991072d
JoaoPedroCavalcanti <joao.cavalcanti.703@gmail.com>
2024-08-14 19:39:28 -0300

    Related fields in serializer

--- appointment/serializer.py
+++ appointment/serializer.py
@@ -1,4 +1,9 @@
 from rest_framework import serializers
+from appointment.models import User
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    username = serializers.CharField(max_length=255)
 
 class AppointmentSerializer(serializers.Serializer):
     id = serializers.IntegerField()
@@ -6,6 +11,12 @@ class AppointmentSerializer(serializers.Serializer):
     date = serializers.DateField()
     public = serializers.BooleanField(source='is_confirmed')
     time_and_date = serializers.SerializerMethodField()
+    # petOwner = serializers.StringRelatedField()
+    
+    # petOwner_id = serializers.PrimaryKeyRelatedField(
+    #     queryset=User.objects.all()
+    # )
+    petOwner = UserSerializer()
     
     def get_time_and_date(self, appointment):
         return f'Time: {appointment.time} and Date: {appointment.date}'
