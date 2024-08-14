09f79c81ea4a2cfb2cfed3f67e4859cca9020720
JoaoPedroCavalcanti <joao.cavalcanti.703@gmail.com>
2024-08-14 17:21:47 -0300

    Using serializer to list appointments

--- appointment/views/api.py
+++ appointment/views/api.py
@@ -1,7 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from appointment.models import Appointment
+from appointment.serializer import AppointmentSerializer
 
 @api_view()
 def appointments_api_list(request):
-    return Response("ok")
+    appointments = Appointment.objects.all()
+    serializer = AppointmentSerializer(instance=appointments, many=True)
+    return Response(serializer.data)
