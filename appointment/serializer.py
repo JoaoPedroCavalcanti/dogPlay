b0526dcc3e00ed10819c933a6965b5b10ecb47d9
JoaoPedroCavalcanti <joao.cavalcanti.703@gmail.com>
2024-08-14 19:01:03 -0300

    How to rename and use serialize methods

--- appointment/serializer.py
+++ appointment/serializer.py
@@ -4,3 +4,8 @@ class AppointmentSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     time = serializers.TimeField()
     date = serializers.DateField()
+    public = serializers.BooleanField(source='is_confirmed')
+    time_and_date = serializers.SerializerMethodField()
+    
+    def get_time_and_date(self, appointment):
+        return f'Time: {appointment.time} and Date: {appointment.date}'
