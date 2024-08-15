5181a3701fc52cca1a07d9676d44ec96c11b50a8
JoaoPedroCavalcanti <joao.cavalcanti.703@gmail.com>
2024-08-15 10:02:42 -0300

    Using ModelSerializer

--- appointment/serializer.py
+++ appointment/serializer.py
@@ -1,22 +1,31 @@
 from rest_framework import serializers
-from appointment.models import User
+from django.contrib.auth.models import User
+from appointment.models import Appointment
 
 class UserSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     username = serializers.CharField(max_length=255)
 
-class AppointmentSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    time = serializers.TimeField()
-    date = serializers.DateField()
-    public = serializers.BooleanField(source='is_confirmed')
-    time_and_date = serializers.SerializerMethodField()
-    # petOwner = serializers.StringRelatedField()
+class AppointmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Appointment
+        fields = ['id', 'time', 'date', 'public', 'time_and_date', 'petOwner', 'petOwner_link']
+        
+    public = serializers.BooleanField(source='is_confirmed', read_only=True)
+    time_and_date = serializers.SerializerMethodField(read_only=True)
     
-    # petOwner_id = serializers.PrimaryKeyRelatedField(
-    #     queryset=User.objects.all()
-    # )
-    petOwner = UserSerializer()
+    petOwner_link = serializers.HyperlinkedRelatedField(
+        many=False,
+        source='petOwner',
+        view_name='appointment:user_detail_api_v1',
+        lookup_field='id',
+        read_only=True
+    )
     
+        
     def get_time_and_date(self, appointment):
         return f'Time: {appointment.time} and Date: {appointment.date}'
+    
+    
+    
+    
--- appointment/views/api.py
+++ appointment/views/api.py
@@ -1,21 +1,26 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from appointment.models import Appointment
-from appointment.serializer import AppointmentSerializer
+from appointment.serializer import AppointmentSerializer, UserSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework import status
+from django.contrib.auth.models import User
+
 
 @api_view()
 def appointments_api_list(request):
     appointments = Appointment.objects.all()
-    serializer = AppointmentSerializer(instance=appointments, many=True)
+    serializer = AppointmentSerializer(instance=appointments, many=True, context={'request': request})
     return Response(serializer.data)
 
 @api_view()
 def appointments_api_detail(request, id):
     appointment = get_object_or_404(Appointment, id=id)
-    serializer = AppointmentSerializer(instance=appointment, many=False)
+    serializer = AppointmentSerializer(instance=appointment, many=False, context={'request': request})
     return Response(serializer.data)
 
-
-    
+@api_view()
+def user_api_detail(request, id):
+    user = get_object_or_404(User, id=id)
+    serializer = UserSerializer(instance=user, many=False, context={'request': request})
+    return Response(serializer.data)
