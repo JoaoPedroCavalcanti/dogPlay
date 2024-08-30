0548b440b812d6db433de63516c196ee45287c60
JoaoPedroCavalcanti <joao.cavalcanti.703@gmail.com>
2024-08-30 13:39:23 -0300

    Desactivating Browsable API

--- dogPlay/settings.py
+++ dogPlay/settings.py
@@ -137,7 +137,10 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 # Django project settings.py
 
