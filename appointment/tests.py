ad9b11bd93d799c5df4e3f2e0f59c4e8da78899d
JoaoPedroCavalcanti <joao.cavalcanti.703@gmail.com>
2024-10-07 14:08:58 -0300

    Creating first test. Test if home url is correct

--- appointment/tests.py
+++ appointment/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse
 
 class AppointmentURLsTest(TestCase):
-    def test_if_pytest_is_ok(self):
-        variavel = '123'
-        assert 1 == 1
+    def test_appointment_home_url_is_correct(self):
+        appointment_home = reverse('appointment:home')
+        self.assertEqual(appointment_home, '/appointment/')
