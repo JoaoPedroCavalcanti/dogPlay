126a6b8cd2e0a189a96969aaad2415c850b6f2df
JoaoPedroCavalcanti <joao.cavalcanti.703@gmail.com>
2024-10-08 09:47:13 -0300

    Tests to test if each route is calling the right function

--- appointment/tests.py
+++ appointment/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from appointment import views
 
 class AppointmentURLsTest(TestCase):
     def test_appointment_home_url_is_correct(self):
@@ -13,3 +14,16 @@ class AppointmentURLsTest(TestCase):
     def test_appointment_myAppointments_url_is_correct(self):
         url = reverse('appointment:myAppointments')
         self.assertEqual(url, '/appointment/my-appointments/')
+        
+class AppointmentViewsTest(TestCase):
+    def test_appointment_home_view_function_is_correct(self):
+        view = resolve(reverse('appointment:home'))
+        self.assertIs(view.func, views.appointmentHome)
+        
+    def test_appointment_confirmation_view_function_is_correct(self):
+        view = resolve(reverse('appointment:confirmation', kwargs= {'id' : 1} ))
+        self.assertIs(view.func, views.appointmentConfirmation)
+        
+    def test_appointment_myAppointments_view_function_is_correct(self):
+        view = resolve(reverse('appointment:myAppointments'))
+        self.assertIs(view.func, views.my_appointments)
