41c7394b7e70cff986936b2ea690946bc6557fa8
JoaoPedroCavalcanti <joao.cavalcanti.703@gmail.com>
2024-10-08 11:43:21 -0300

    Using self.client to test status code accessing home page and if it returns the right template

--- appointment/tests/test_appointment_views.py
+++ appointment/tests/test_appointment_views.py
@@ -14,3 +14,11 @@ class AppointmentViewsTest(TestCase):
     def test_appointment_myAppointments_view_function_is_correct(self):
         view = resolve(reverse('appointment:myAppointments'))
         self.assertIs(view.func, views.my_appointments)
+        
+    def test_appointment_home_view_function_returns_code_200(self):
+        response = self.client.get(reverse('appointment:home'))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_appointment_home_view_function_load_correct_template(self):
+        response = self.client.get(reverse('appointment:home'))
+        self.assertTemplateUsed(response, 'appointment/pages/appointmentHome.html')
